5d469fb6b2fca0124892ad98242d77560a98bbb2
Gro0ve <vassilistotskas@msn.com>
2023-10-17 21:15:06 +0300

    feat(ci): Use node version 21

--- build/dist/src/MediaStream/Job/StoreResourceResponseToFileJob.d.ts
+++
@@ -1,4 +0,0 @@
-import { AxiosResponse } from 'axios';
-export default class StoreResourceResponseToFileJob {
-    handle(resourceName: string, path: string, response: AxiosResponse): Promise<void>;
-}
